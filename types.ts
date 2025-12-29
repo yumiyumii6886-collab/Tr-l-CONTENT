49df0034f4c1dad73e5107b5fd0754c55faeed08
yumiyumii6886-collab <yumiyumii6886@gmail.com>
2025-12-29 19:14:14 +0700

    Create types.ts

---
+++ types.ts
@@ -0,0 +1,12 @@
+
+export interface CompanyInfo {
+  name: string;
+  hotline: string;
+  address: string;
+}
+
+export interface AdContent {
+  headline: string;
+  body: string;
+  hashtags: string[];
+}
