a8dcbbf492c5116df6745b6ae613510c09db02eb
yumiyumii6886-collab <yumiyumii6886@gmail.com>
2025-12-29 19:24:09 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    // Cho phép sử dụng process.env.API_KEY trên Vercel
-    'process.env': process.env
+    'process.env': {}
   },
   build: {
     target: 'esnext',
