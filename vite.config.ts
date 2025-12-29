7bb860087703416f42d3c7b65c2fefbab05b5252
yumiyumii6886-collab <yumiyumii6886@gmail.com>
2025-12-29 22:07:10 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // Tạo "cây cầu" để truyền API_KEY từ Vercel vào code chạy trên điện thoại
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+  },
   build: {
     target: 'esnext',
     outDir: 'dist',
